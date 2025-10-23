abae8c8ebdebffbba094c3b62a6f25f5e6aa62f7
VRithea <vongrithea126@gmail.com>
2025-10-24 01:03:53 +0700

    Remove Unused Questions

--- lib/main.dart
+++ lib/main.dart
@@ -5,21 +5,6 @@ import 'domain/quiz.dart';
 import 'ui/quiz_console.dart';
 
 void main() {
-
-  List<Question> questions = [
-    Question(
-        title: "Capital of France?",
-        choices: ["Paris", "London", "Rome"],
-        goodChoice: "Paris"    
-    ),
-    Question(
-        title: "2 + 2 = ?", 
-        choices: ["2", "4", "5"], 
-        goodChoice: "4",
-        points: 50,
-    ),
-  ];
-
   QuizRepository repo = new QuizRepository('./lib/data/quiz.json');
   Quiz quiz = repo.readQuiz();
 
